b40f202228a170b1f07b44d1f21e3591d28cfdfb
ithurtstom <ithurtstom@gmail.com>
2013-11-20 09:15:52 +0100

    updated connective model

--- src/ConnectiveModel.cpp
+++ src/ConnectiveModel.cpp
@@ -107,30 +107,76 @@ struct ConnectiveModelState : public FeatureFunction::State, public FeatureFunct
 
 	Float matchConnectiveDictionary(const AnchoredPhrasePair& pair) {
 
-		 static const boost::regex regSource("(^|\\s)((A|a)fter all|(A|a)fter|(A|a)lso|(A|a)lthough|(B|b)ut|(B|b)ecause|(E|e)ven though|(I|i)nstead|(S|s)ince|(T|t)hough|(M|m)eanwhile|(W|w)hile|(Y|y)et|(H|h)owever|(W|w)hen|(E|e)ven if|(A|a)s if|(I|i)f|(A|a)s soon as|(A|a)s much as|(A|a)s far as|(A|a)s well as|(A|a)s fast as|(J|j)ust as|(A|a)s regards|(A|a)s long as|(A|a)s a result|(B|b)efore|(T|t)hen|(S|s)till|(U|u)ntil|(T|t)hus|(I|i)n addition|(U|u)nless|(I|i)ndeed|(M|m)oreover|(I|i)n fact|(L|l)ater|(F|f)or example|(O|o)nce|(S|s)eparately|(P|p)reviously|(F|f)inally|(N|n)evertheless|(N|n)onetheless|(B|b)y contrast|(O|o)n the other hand|(S|s)o that|(G|g)iven that|(N|n)ow that|(T|t)herefore|(O|o)therwise|(F|f)or instance|(I|i)n turn|(A|a)s)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*");
-		 boost::match_results<std::string::const_iterator> resultsSrc;
+		 const std::vector<std::string>& source_phrase = pair.second.get().getSourcePhrase().get();
+		 const std::vector<std::string>& target_phrase = pair.second.get().getTargetPhrase().get();
 
-		for (uint j = 0; j < pair.second.get().getSourcePhrase().get().size(); j++) {
-			 if (boost::regex_match(pair.second.get().getSourcePhrase().get()[j], resultsSrc, regSource)) {
-				 	std::string myMatch = resultsSrc[2];
-				 	LOG(logger_, debug, "Regex matches: " << myMatch);
-				 	static const boost::regex regTarget(findTargetRegex(myMatch));
-				 	boost::match_results<std::string::const_iterator> resultsTrg;
-				 	if (!pair.second.get().getTargetPhrase().get()[j].empty()) {
-				    if (!boost::regex_match(pair.second.get().getTargetPhrase().get()[j], resultsTrg, regTarget)) {
-				 		 fin++;
-				 		 LOG(logger_, debug, "Increase for: " << pair.second.get().getSourcePhrase().get()[j] << " -- " << pair.second.get().getTargetPhrase().get()[j]);
-				 	} else {
-				 	     fin--;
-				 	     LOG(logger_, debug, "Decrease for: " << pair.second.get().getSourcePhrase().get()[j] << " -- deleting: " << pair.second.get().getTargetPhrase().get()[j]);
-				 	     removePhrasePair(pair); // want to delete (or downscore) the target phrase that actually contains a valid connective translation
-				 	}
-				 	} else {
-				 		continue;
-				 	}
-			 }
+		std::string multiSrcWord;
+		std::string multiTrgWord;
+
+		 if (source_phrase.size() <= target_phrase.size()) {
+		 // source phrase is smaller
+			 for (uint j = 0; j < source_phrase.size(); j++) {
+				if (j > 3) {
+					multiSrcWord = source_phrase[j-3]+source_phrase[j-2]+source_phrase[j-1]+source_phrase[j];
+					multiTrgWord = target_phrase[j-3]+target_phrase[j-2]+target_phrase[j-1]+target_phrase[j];
+				}
+				else if (j > 2) {
+					multiSrcWord = source_phrase[j-2]+source_phrase[j-1]+source_phrase[j];
+					multiTrgWord = target_phrase[j-2]+target_phrase[j-1]+target_phrase[j];
+				}
+				else if (j > 1) {
+					multiSrcWord = source_phrase[j-1]+source_phrase[j];
+					multiTrgWord = target_phrase[j-1]+target_phrase[j];
+				}
+				else {
+						multiSrcWord = source_phrase[j];
+						multiTrgWord = target_phrase[j];
+				}
+				fin = matching(multiSrcWord,multiTrgWord,pair);
+		 }
+		 } else {
+			 // target phrase is smaller
+			 for (uint j = 0; j < target_phrase.size(); j++) {
+			 				if (j > 3) {
+			 					multiSrcWord = source_phrase[j-3]+source_phrase[j-2]+source_phrase[j-1]+source_phrase[j];
+			 					multiTrgWord = target_phrase[j-3]+target_phrase[j-2]+target_phrase[j-1]+target_phrase[j];
+			 				}
+			 				else if (j > 2) {
+			 					multiSrcWord = source_phrase[j-2]+source_phrase[j-1]+source_phrase[j];
+			 					multiTrgWord = target_phrase[j-2]+target_phrase[j-1]+target_phrase[j];
+			 				}
+			 				else if (j > 1) {
+			 					multiSrcWord = source_phrase[j-1]+source_phrase[j];
+			 					multiTrgWord = target_phrase[j-1]+target_phrase[j];
+			 				}
+			 				else {
+			 						multiSrcWord = source_phrase[j];
+			 						multiTrgWord = target_phrase[j];
+			 				}
+			 				fin = matching(multiSrcWord, multiTrgWord,pair);
+			 		 }
 		 }
 		LOG(logger_, debug, "ConnectiveModel score: " << fin);
+		return fin;
+	}
+
+	Float matching (const std::string& s, const std::string& t, const AnchoredPhrasePair& p) {
+		 static const boost::regex regSource("(^|\\s)((A|a)fter all|(A|a)fter|(A|a)lso|(A|a)lthough|(B|b)ut|(B|b)ecause|(E|e)ven though|(I|i)nstead|(S|s)ince|(T|t)hough|(M|m)eanwhile|(W|w)hile|(Y|y)et|(H|h)owever|(W|w)hen|(E|e)ven if|(A|a)s if|(I|i)f|(A|a)s soon as|(A|a)s much as|(A|a)s far as|(A|a)s well as|(A|a)s fast as|(J|j)ust as|(A|a)s regards|(A|a)s long as|(A|a)s a result|(B|b)efore|(T|t)hen|(S|s)till|(U|u)ntil|(T|t)hus|(I|i)n addition|(U|u)nless|(I|i)ndeed|(M|m)oreover|(I|i)n fact|(L|l)ater|(F|f)or example|(O|o)nce|(S|s)eparately|(P|p)reviously|(F|f)inally|(N|n)evertheless|(N|n)onetheless|(B|b)y contrast|(O|o)n the other hand|(S|s)o that|(G|g)iven that|(N|n)ow that|(T|t)herefore|(O|o)therwise|(F|f)or instance|(I|i)n turn|(A|a)s)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*");
+		 boost::match_results<std::string::const_iterator> resultsSrc;
+		 if (boost::regex_match(s, resultsSrc, regSource)) {
+			 	std::string myMatch = resultsSrc[2];
+			 	LOG(logger_, debug, "Regex matches: " << myMatch);
+			 	static const boost::regex regTarget(findTargetRegex(myMatch));
+			 	boost::match_results<std::string::const_iterator> resultsTrg;
+			    if (!boost::regex_match(t, resultsTrg, regTarget)) {
+			 		 fin++;
+			 		 LOG(logger_, debug, "Increase for: " << s << " -- " << t);
+			 	} else {
+			 	     fin--;
+			 	     LOG(logger_, debug, "Decrease for: " << s << " -- deleting: " << t);
+			 	     removePhrasePair(p); // want to delete (or downscore) the target phrase that actually contains a valid connective translation
+			 	}
+		 }
 		return fin;
 	}
 
@@ -145,9 +191,9 @@ struct ConnectiveModelState : public FeatureFunction::State, public FeatureFunct
 		t2s.removePhrasePair(t,s);
 	}
 
-	static std::string findTargetRegex(std::string& s) {
+	static std::string findTargetRegex(const std::string& s) {
 
-		std::string targetRegex = "bla";
+		std::string targetRegex;
 
 		if (s == "After all" || s == "after all")
 			targetRegex = "(^|\\s)(schliesslich|trotz allem)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
@@ -176,7 +222,7 @@ struct ConnectiveModelState : public FeatureFunction::State, public FeatureFunct
 		else if (s == "Yet" || s == "yet")
 			targetRegex = "(^|\\s)(dennoch|aber|bisher|trotzdem|jedoch|bislang|erneut|dabei|gleichzeitig|obwohl|andererseits|bereits|bisher noch|derzeit|zunächst einmal|zunächsteinmal|einmal|nach wie vor|nochmals|wenngleich|abermals|bis jetzt|bisjetzt|bisweilen|dagegen|indes|selbst wenn|während|noch immer|noch|doch)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "However" || s == "however")
-			targetRegex = "^(^|\\s)(jedoch|aber|dagegen|durchaus|indes|hoffentlich|ausserdem|natürlich|allenfalls|andererseits|allerdings|dennoch|trotzdem|hingegen|obwohl|gleichwohl|dabei|jedenfalls|nichtsdestotrotz|nichtsdestoweniger|dessenungeachtet|unterdessen|indessen|nun|zwar|doch|trotz)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
+			targetRegex = "(^|\\s)(jedoch|aber|dagegen|durchaus|indes|hoffentlich|ausserdem|natürlich|allenfalls|andererseits|allerdings|dennoch|trotzdem|hingegen|obwohl|gleichwohl|dabei|jedenfalls|nichtsdestotrotz|nichtsdestoweniger|dessenungeachtet|unterdessen|indessen|nun|zwar|doch|trotz)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "When" || s == "when")
 			targetRegex = "(^|\\s)(als|wo|da|wobei|wenn|falls|wann|sobald|während|ob|bis|dann|ehe|neben|nachdem|beim|nach|bei)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "As if" || s == "as if")
@@ -204,7 +250,7 @@ struct ConnectiveModelState : public FeatureFunction::State, public FeatureFunct
 		else if (s == "As a result" || s == "as a result")
 			targetRegex = "(^|\\s)(als Ergebnis|dadurch|daher|folglich|daraufhin|demzufolge|dabei|infolgedessen|so|als Folge)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "As long as" || s == "as long as")
-			targetRegex = "^(^|\\s)(solange wie|solange|solang|sofern|vorausgesetzt, dass|vorausgesetzt)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
+			targetRegex = "(^|\\s)(solange wie|solange|solang|sofern|vorausgesetzt)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "As soon as" || s == "as soon as")
 			targetRegex = "(^|\\s)(wenn|sobald wie|so bald wie|sobald|sowie)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "As" || s == "as")
@@ -222,7 +268,7 @@ struct ConnectiveModelState : public FeatureFunction::State, public FeatureFunct
 		else if (s == "In addition" || s == "in addition")
 			targetRegex = "(^|\\s)(zusätzlich|ausserdem|dazu|daneben|ferner|hinzu|des Weiteren|zuzüglich|ausserdem|nicht nur|darüber hinaus|darüberhinaus|ausser|auch|und|noch)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "Unless" || s == "unless")
-			 targetRegex = "(^|\\s)(ausser wenn|es sei denn|sofern nicht|sofern|wenn nicht|falls nicht|solange wie|vorausgesetzt, dass|ausgenommen dass|wenn)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
+			 targetRegex = "(^|\\s)(ausser wenn|es sei denn|sofern nicht|sofern|wenn nicht|falls nicht|solange wie|vorausgesetzt|ausgenommen dass|wenn)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "Indeed" || s == "indeed")
 			targetRegex = "(^|\\s)(in der Tat|zwar|ja|allerdings|gewiss|tatsächlich|wirklich|wohl|wahrlich|freilich)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "Moreover" || s == "moreover")
@@ -230,7 +276,7 @@ struct ConnectiveModelState : public FeatureFunction::State, public FeatureFunct
 		else if (s == "In fact" || s == "in fact")
 			targetRegex = "(^|\\s)(tatsächlich|in der Tat|eigentlich|und zwar|vielmehr|in Wirklichkeit|wahrhaftig|so gesehen|sogar|faktisch|übrigens|in Wahrheit)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "Later" || s == "later")
-			targetRegex = "^(^|\\s)(später|nachträglich|nachher|nachmals|darauf|nach)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
+			targetRegex = "(^|\\s)(später|nachträglich|nachher|nachmals|darauf|nach)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "For example" || s == "for example")
 			targetRegex = "(^|\\s)(zum Beispiel|beispielsweise|etwa|unter anderem|so)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "For instance" || s == "for instance")
@@ -256,15 +302,15 @@ struct ConnectiveModelState : public FeatureFunction::State, public FeatureFunct
 		else if (s == "So that" || s == "so that")
 			targetRegex = "(^|\\s)(so dass|damit|sodass|so|weshalb)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "Given that" || s == "given that")
-			targetRegex = "(^|\\s)(zumal|aufgrund|weil|angesichts|wenn man bedenkt, dass|da)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
+			targetRegex = "(^|\\s)(zumal|aufgrund|weil|angesichts|wenn man bedenkt|da)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "Now that" || s == "now that")
 			targetRegex = "(^|\\s)(aufgrund|nachdem|jetzt|nunmehr|nun|nachdem|da|währenddessen|obwohl)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "Therefore" || s == "therefore")
 			targetRegex = "(^|\\s)(daher|deshalb|somit|also|deswegen|darum|folglich|dafür|hierfür|demzufolge|mithin|ergo|aus diesem Grund|damit|demnach|infolgedessen)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
 		else if (s == "Otherwise" || s == "otherwise")
 			targetRegex = "(^|\\s)(ansonsten|andernfalls|anderweitig|anderenfalls|anderweit|andererseits|widrigenfalls|andrerseits|anders|sonst)(\\s|\\,|\\.|\\?|\\!|\\:|\\;|\\'|\\’).*";
-		else
-			targetRegex = "bla";
+		//else
+			//targetRegex = "bla";
 		return targetRegex;
 	}
 
